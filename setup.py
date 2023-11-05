642195ac8f7f7ee70c7b441b9648c88a40e80562
Shahbaz Mahmood <shahbazmahmood@Shahbazs-MacBook-Pro.local>
2023-11-04 20:56:45 -0400

    changing setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
-DESCRIPTION = 'A tower sdk'
+DESCRIPTION = 'A SDK that helps debug batch compute enviornments in AWS, GCP and Azure and Kubernetes'
 LONG_DESCRIPTION = 'A package that makes it easier to debug issues with batch compute enviornments in GCP, AWS and Azure'
 
 setup(
-    name="tower_sdk",
+    name="batch_debugging",
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
