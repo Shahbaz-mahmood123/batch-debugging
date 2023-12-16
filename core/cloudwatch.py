26af2b7137984898308c125e1a8b426f91fd48c3
Shahbaz Mahmood <shahbazmahmood@Shahbazs-MacBook-Pro.local>
2023-12-15 21:27:36 -0500

    forgot to remove print and and

--- core/cloudwatch.py
+++ core/cloudwatch.py
@@ -15,7 +15,6 @@ class CloudWatch(CloudWatchInterface):
     def get_log_groups(self) -> LogGroupResponse:  
         try:
             log_groups = self.cloudwatch_client.describe_log_groups()
-            print(log_groups)
         except Exception as e:
             return f"An error occured fetching the log groups: {e}"
         finally:
@@ -23,17 +22,15 @@ class CloudWatch(CloudWatchInterface):
             return log_group_response 
         
     def get_log_streams(self, log_group: LogGroup=None ,log_group_name=None) -> LogStreamResponse:
-        print(log_group)
-        
+
         if log_group_name is None:
             log_group_name = log_group.logGroupName
-        elif log_group is None & log_group_name is None:
+        elif log_group is None and log_group_name is "":
             return f"Please pass either a log group object or the log group name"
         
         if "tower/forge" in log_group_name:
             try: 
                 log_stream = self.cloudwatch_client.describe_log_streams(logGroupIdentifier=log_group_name, orderBy="LastEventTime")
-                pprint(log_stream.firstEventTimestamp)
             except Exception as e:
                 return f"An error occured fetching the logstream {e}"   
             finally:
