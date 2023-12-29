bb6c657e271351c505c9a3409745982619e56851
Shahbaz Mahmood <shahbazmahmood@Shahbazs-MacBook-Pro.local>
2023-12-28 20:55:29 -0500

    working file download to vm and service accounts

---
+++ infrastructure/seqera_platform.py
@@ -0,0 +1,34 @@
+class ISeqeraConfig():
+    def fetch_secrets():
+        pass 
+    
+    def parse_config():
+        pass 
+     
+
+class SeqeraConfig(ISeqeraConfig):
+    
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+        
+    def fetch_secrets(self):
+        pass 
+    
+    def parse_config(self):
+        pass 
+    
+    
+class SeqeraGCPConfig(SeqeraConfig):
+    
+    def __init__(self, file_path: str) -> None:
+       self.file_path = file_path
+       super().__init__(file_path)
+       
+    
+    def fetch_secrets(self):
+        pass
+    
+    def parse_config(self):
+        pass
+
+    
