30e4660cafc8fe7b0d29f3dd6b31f6c5c5352cc3
Shahbaz Mahmood <shahbazmahmood@Shahbazs-MacBook-Pro.local>
2024-01-01 17:19:54 -0500

    inital gke cluster class

--- infrastructure/gcp_gke_cluster.py
+++ infrastructure/gcp_gke_cluster.py
@@ -9,17 +9,57 @@ class PulumiGKEInterface():
 
 class PulumiGKE(PulumiInfraConfig, PulumiGKEInterface):
     
-    def __init__(self, project_id: str, name: str, region:str , zone: str, cluster_name: str, cluster_type: str, nodes: dict) -> None:
+    def __init__(self, project_id: str, name: str, region:str , zone: str, cluster_name: str, cluster_type: str, nodes: dict ,node_count: int) -> None:
         self.project_id = project_id 
         self.name = name
         self.region = region 
         self.zone = zone
         self.cluster_name = cluster_name
         self.cluster_type = cluster_type
-        self.nodes = nodes
+        self.node_count = node_count
+        self.nodes = nodes 
         
     def pulumi_program(self):
-        pass
-    
+        # Create a GKE cluster
+        gke_cluster = gcp.container.Cluster(self.cluster_name,
+            initial_node_count=1,
+            # Define the node config for the cluster
+            node_config=gcp.container.ClusterNodeConfigArgs(
+                machine_type="n1-standard-1",  # Standard machine type
+                oauth_scopes=[
+                    "https://www.googleapis.com/auth/compute",
+                    "https://www.googleapis.com/auth/devstorage.read_only",
+                    "https://www.googleapis.com/auth/logging.write",
+                    "https://www.googleapis.com/auth/monitoring"
+                ],
+            ),
+            remove_default_node_pool=True,
+            # Define the location for the cluster
+            location=self.region
+        )
+
+        # Create a custom node pool attached to the GKE cluster created above.
+        custom_node_pool = gcp.container.NodePool("my-custom-node-pool",
+            location=gke_cluster.location,
+            cluster=gke_cluster.name,
+            initial_node_count=1,
+            node_config=gcp.container.NodePoolNodeConfigArgs(
+                machine_type="n1-standard-1", # Specify the machine type for the nodes.
+            ),
+            autoscaling=gcp.container.NodePoolAutoscalingArgs(
+                min_node_count=1,
+                max_node_count=3,
+            ),
+            management=gcp.container.NodePoolManagementArgs(
+                auto_repair=True,
+                auto_upgrade=True
+            ))
+        # Export the Cluster Name
+        pulumi.export("cluster_name", gke_cluster.name)
+        # Export the Cluster Endpoint
+        pulumi.export("cluster_endpoint", gke_cluster.endpoint)
+        # Export the Cluster Master Version
+        pulumi.export("cluster_master_version", gke_cluster.master_version)
+            
     def get_secrets(self):
         pass
