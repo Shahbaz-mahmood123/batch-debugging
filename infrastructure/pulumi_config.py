ca7b9e7d9b515b201a2f37f13afcc12a40369020
Shahbaz Mahmood <shahbazmahmood@Shahbazs-MacBook-Pro.local>
2024-01-17 19:35:46 -0500

    changing names and restructure

--- infrastructure/pulumi_config.py
+++ infrastructure/pulumi_config.py
@@ -19,7 +19,37 @@ class PulumiConfig(PulumiConfigInterface):
     def validate_yaml(self):
         pass
         
-class PulumiGCPConfig(PulumiConfig):
+class MinimalPulumiGCPConfig(PulumiConfig):
+    
+    def __init__(self, file_path: str) -> None:
+        
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError(f"YAML file not found: {file_path}")
+
+        with open(file_path, 'r') as file:
+            self.configs = yaml.safe_load(file) 
+        
+        self.location = self.configs['location']
+        self.resource_name = self.configs['name']
+        self.project_id = self.configs['project_id']
+        self.zone = self.configs['zone']
+        self.region = self.configs['region']
+        self.instance_name = self.configs['instance_name']
+        self.tower_env_secret = self.configs.get('secrets', {}).get('tower_env_secret', []) 
+        self.tower_yaml_secret = self.configs.get('secrets', {}).get('tower_yaml_secret', []) 
+        self.harbor_creds = self.configs.get('secrets', {}).get('harbor_creds', [])  
+        self.groundswell_secret = self.configs.get('secrets', {}).get('groundswell_secret', []) 
+        self.source_ranges = self.configs.get('network', {}).get('source_ranges', [])
+        self.tags = self.configs.get('compute-engine', {}).get('tags', [])
+        self.source_tags = self.configs.get('network', {}).get('tags', [])
+
+        super().__init__(file_path)
+    
+    ##TODO: Need to do some validation on the yaml maybe? 
+    def validate_yaml(self):
+        pass
+    
+class StandardMinimalPulumiGCPConfig(PulumiConfig):
     
     def __init__(self, file_path: str) -> None:
         
